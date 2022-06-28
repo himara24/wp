f9bb7cbbbb4ad21e5163178ac1139341feb33ee9
himara24 <himarashanaka425@gmail.com>
2022-06-28 08:52:20 +0530

    Fist Commit

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function followandrew_register_styles(){
+
+    $version = wp_get_theme()->get( 'Version' );
+    wp_enqueue_style('followandrew-style', get_template_directory_uri() . "/style.css" , array(), $version , 'all');
+    wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" , array(), '4.4.1', 'all');
+    wp_enqueue_style('followandrew-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" , array(), '5.13.0', 'all');
+}  
+
+add_action("wp_enqueue_scripts", 'followandrew_register_styles' );
+
+
+
+function followandrew_register_scripts(){
+    
+     wp_enqueue_script("himara-bootstrap" , "https://code.jquery.com/jquery-3.4.1.slim.min.js" , array(), "3.4.1");
+
+    
+}  
+
+add_action("wp_enqueue_scripts", 'followandrew_register_scripts' );
+
+
+
+?>
