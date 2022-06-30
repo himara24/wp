11c72f7a4d0b45699ca260ad8ea0f5989fcfcade
himara24 <himarashanaka425@gmail.com>
2022-06-30 12:25:16 +0530

    second

--- functions.php
+++ functions.php
@@ -1,5 +1,31 @@
 <?php
 
+
+function himara_theme_support(){
+
+    // Add Dynamic Title tag support
+    add_theme_support("title-tag");
+}
+
+add_action("after_setup_theme", "himara_theme_support");
+
+
+
+function himara_menus(){
+
+    $locations = array(
+        "primary" => "Desktop Primary Left Sidebar",
+        "footer" => "Footer Menu Items"
+
+    );
+
+    register_nav_menus($locations);
+
+};
+
+add_action("init", "himara_menus");
+
+
 function followandrew_register_styles(){
 
     $version = wp_get_theme()->get( 'Version' );
@@ -14,13 +40,15 @@ add_action("wp_enqueue_scripts", 'followandrew_register_styles' );
 
 function followandrew_register_scripts(){
     
-     wp_enqueue_script("himara-bootstrap" , "https://code.jquery.com/jquery-3.4.1.slim.min.js" , array(), "3.4.1");
+     wp_enqueue_script("himara-bootstrap" , "https://code.jquery.com/jquery-3.4.1.slim.min.js" , array(), "3.4.1", true );
+
+     wp_enqueue_script("himara-propper" , "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" , array(), "1.16", true );
+
+     wp_enqueue_script("himara-min" , "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" , array(), "4.4.1", true );
+
+     wp_enqueue_script("himara-main", get_template_directory(). "/assets/js/main.js" , array(), "1.0", true );
 
     
 }  
 
 add_action("wp_enqueue_scripts", 'followandrew_register_scripts' );
-
-
-
-?>
